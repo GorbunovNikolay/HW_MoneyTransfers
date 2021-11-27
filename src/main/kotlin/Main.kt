7150e08920c3464a9b086a63f6b3ba38cf671c61
Gorbunov Nikolay <gor_bunk@mail.ru>
2021-11-27 14:31:03 +0300

    Добавлено решение задачи

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val percentСommissionFees = 0.75        //комиссионные сборы 0.75%, но не менее 35 руб
+    val minCommission = 35                  //сумма минимальной комиссии
+    var amount = 30000.98                   //сумма перевода в копейках 30000 руб., 98 коп.
+
+    var сommission = (amount * percentСommissionFees) / 100
+
+    //округляем сумму комиссии
+    var df = DecimalFormat("#.##")
+    df.roundingMode = RoundingMode.CEILING
+    var result = df.format(сommission)
+
+    //проверяем сумму комиссии, она не может быть меньше минимальной комиссии, если меньше то она равна минимальной
+    if (сommission < minCommission) {
+        сommission = minCommission.toDouble()
+    }
+
+    val charRub = '\u20BD'                      //символ рубля
+    println("Комиссия составила: $result $charRub")
 }
